9414a772d0f04cd9a28673b349afdb0711c189ac
lotarion <danyil.bazarov@gmail.com>
2022-08-30 20:49:17 +0300

    new addBasics note

---
+++ app/src/main/java/com/lotsqd/kotlin_basics/addBasics/addBasics_7.kt
@@ -0,0 +1,9 @@
+package com.lotsqd.kotlin_basics.addBasics
+
+fun main() {
+    val location: Any = "Kotlin"
+    val safeString: String? = location as? String
+    val safeInt: Int? = location as? Int
+    println(safeString)
+    println(safeInt)
+}
